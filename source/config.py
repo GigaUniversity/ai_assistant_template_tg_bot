3b0ec2de15426a6845d012fc0d416990b0145d81
ralendo <xralendo@gmail.com>
2024-05-02 19:32:12 +0200

    v0.2.2

--- source/config.py
+++ source/config.py
@@ -17,7 +17,7 @@ class Config:
     id_of_uni = os.environ.get('UNI_ID')
     url_to_api = os.environ.get('URL_TO_API')
     admin_id = os.environ.get('ADMIN_ID')
-    project_dir = os.path.dirname(os.path.abspath(__file__))
+    project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     styles_of_dialog = asyncio.run(get_settings(project_dir=project_dir))['styles_of_dialog']
     commands_for_bot = asyncio.run(get_settings(project_dir=project_dir))['commands_for_bot']
 
