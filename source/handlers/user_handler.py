99483bbff40a1b496378998f7aadcbf44463853c
ralendo <xralendo@gmail.com>
2024-05-02 16:30:10 +0200

    v0.2.1 fix response

--- source/handlers/user_handler.py
+++ source/handlers/user_handler.py
@@ -63,9 +63,6 @@ async def button_show_relevant_links(call: CallbackQuery, bot: Bot, state: FSMCo
     data = await state.get_data()
     all_responses = data.get('all_responses')
     response = all_responses.get(str(call.message.message_id))
-    logger.debug(call.message.message_id)
-    logger.debug(response)
-    logger.debug(all_responses)
 
     text = service_messages.show_relevant_links()
     await call.answer()
--- source/start.py
+++ source/start.py
@@ -6,6 +6,7 @@ from aiogram.client.bot import DefaultBotProperties
 
 from source.config import Config
 from source.handlers.user_handler import router
+from source.utils.logger_settings import logger
 
 default_properties = DefaultBotProperties(parse_mode=ParseMode.HTML)
 bot = Bot(token=Config.bot_token, default=default_properties)
@@ -15,18 +16,19 @@ dp.include_router(router)
 
 async def on_startup_echo():
     text = 'Bot is started'
+    logger.info(f'Bot for UNI "{Config.name_of_uni}" is started')
     await bot.send_message(chat_id=Config.admin_id,
                            text=text)
 
 
 async def on_shutdown_echo():
     text = 'Bot is shutdown'
+    logger.info(f'Bot for UNI "{Config.name_of_uni}" is shutdown')
     await bot.send_message(chat_id=Config.admin_id,
                            text=text)
 
 
 async def main():
-    print('Start bot')
     try:
         await bot.delete_webhook(drop_pending_updates=True)
         dp.startup.register(on_startup_echo)
