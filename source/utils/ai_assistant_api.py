c7e2d96c3a41c7f7adc79d38de453a19741d9a38
ralendo <xralendo@gmail.com>
2024-06-11 14:30:01 +0200

    fix

--- source/utils/ai_assistant_api.py
+++ source/utils/ai_assistant_api.py
@@ -5,11 +5,11 @@ headers = {'Authorization': 'Bearer ' + Config.ACCESS_TOKEN}
 
 async def get_query(params: dict, endpoint: str):
     async with aiohttp.ClientSession() as session:
-        async with session.get(url=f"{Config.URL_TO_API}{endpoint}", params=params) as response:
+        async with session.get(headers=headers, url=f"{Config.URL_TO_API}{endpoint}", params=params) as response:
             return response.status, await response.json()
 
 
 async def post_query(params: dict, endpoint: str):
     async with aiohttp.ClientSession() as session:
-        async with session.post(url=f"{Config.URL_TO_API}{endpoint}", params=params) as response:
+        async with session.post(headers=headers, url=f"{Config.URL_TO_API}{endpoint}", params=params) as response:
             return response.status
